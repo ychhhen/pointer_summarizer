5fea8cf46154c2b87bac4ff27ad139758dc04657
Ashish Baghudana <ashish.baghudana26@gmail.com>
2018-11-06 11:15:33 -0500

    Modify start evaluation script

--- start_eval.sh
+++ start_eval.sh
@@ -1,5 +1,4 @@
-export PYTHONPATH=`pwd`
 MODEL_PATH=$1
 MODEL_NAME=$(basename $MODEL_PATH)
-python training_ptr_gen/eval.py $MODEL_PATH >& ../log/eval_log.$MODEL_NAME &
+python training_ptr_gen/eval.py $MODEL_PATH >& log/eval_log.$MODEL_NAME &
 
